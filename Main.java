464ec2bf715c891f7129a730e2d13f9ef839ce9e
alt-ian <2864730@gmail.com>
2026-03-17 17:35:05 +0100

    metodo fechaHora

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
+
+import java.time.LocalDateTime;
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+
+    public void fechaHora() {
+        LocalDateTime ahora = LocalDateTime.now();
+        System.out.println("Fecha y hora actuales: " + ahora);
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Mi-nuevo-Proyecto
 En este proyecto se verá mi tarea de entornos de desarrollo.
+
+# Main
+Metodo principal del programa que te muestra un hola mundo.
+
+# Metodo fechaHora
+Metodo que te muestra la hora y la fecha actuales.
